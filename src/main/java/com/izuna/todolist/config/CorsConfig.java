bfec980779ea8eb293d4c01d2b7123f5b4506223
issamfladi <issam.fladi@gmail.com>
2024-02-02 22:34:06 +0100

    cors

--- src/main/java/com/izuna/todolist/config/CorsConfig.java
+++ src/main/java/com/izuna/todolist/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("api/todos/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*");
--- src/main/java/com/izuna/todolist/web/TodoRestController.java
+++ src/main/java/com/izuna/todolist/web/TodoRestController.java
@@ -12,7 +12,6 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/todos")
-@CrossOrigin(origins = "http://localhost:3000")
 public class TodoRestController {
 
     private final TodoService todoService;
