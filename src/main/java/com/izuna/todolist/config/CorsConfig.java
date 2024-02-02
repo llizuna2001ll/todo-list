34f6f47f98b77010d3b265d731bac5b84d1112e2
issamfladi <issam.fladi@gmail.com>
2024-02-03 00:38:50 +0100

    cors

--- src/main/java/com/izuna/todolist/config/CorsConfig.java
+++ src/main/java/com/izuna/todolist/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("api/todos/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedMethods("*")
                 .allowedHeaders("*");
     }
 }
